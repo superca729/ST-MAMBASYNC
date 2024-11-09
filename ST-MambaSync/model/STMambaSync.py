b8d4fe3dce493458784ceedc7030e2e4727ef772
Zhiqi Jessica Shao <54494470+superca729@users.noreply.github.com>
2024-11-09 15:00:26 +1100

    Add files via upload

---
+++ ST-MambaSync/model/STMambaSync.py
@@ -0,0 +1,414 @@
+import torch.nn as nn
+import torch
+from torchinfo import summary
+import math
+import torch.nn.functional as F
+from mamba_ssm import Mamba
+import os
+import matplotlib.pyplot as plt
+from sklearn.manifold import Isomap
+import numpy as np
+class AttentionLayer(nn.Module):
+    """Perform attention across the -2 dim (the -1 dim is `model_dim`).
+
+    Make sure the tensor is permuted to correct shape before attention.
+
+    E.g.
+    - Input shape (batch_size, in_steps, num_nodes, model_dim).
+    - Then the attention will be performed across the nodes.
+
+    Also, it supports different src and tgt length.
+
+    But must `src length == K length == V length`.
+
+    """
+
+    def __init__(self, model_dim, num_heads=8, mask=False):
+        super().__init__()
+
+        self.model_dim = model_dim
+        self.num_heads = num_heads
+        self.mask = mask
+
+        self.head_dim = model_dim // num_heads
+
+        self.FC_Q = nn.Linear(model_dim, model_dim)
+        self.FC_K = nn.Linear(model_dim, model_dim)
+        self.FC_V = nn.Linear(model_dim, model_dim)
+
+        self.out_proj = nn.Linear(model_dim, model_dim)
+
+    def forward(self, query, key, value):
+        # Q    (batch_size, ..., tgt_length, model_dim)
+        # K, V (batch_size, ..., src_length, model_dim)
+        batch_size = query.shape[0]
+        tgt_length = query.shape[-2]
+        src_length = key.shape[-2]
+
+        query = self.FC_Q(query)
+        key = self.FC_K(key)
+        value = self.FC_V(value)
+
+        # Qhead, Khead, Vhead (num_heads * batch_size, ..., length, head_dim)
+        query = torch.cat(torch.split(query, self.head_dim, dim=-1), dim=0)
+        key = torch.cat(torch.split(key, self.head_dim, dim=-1), dim=0)
+        value = torch.cat(torch.split(value, self.head_dim, dim=-1), dim=0)
+
+        key = key.transpose(
+            -1, -2
+        )  # (num_heads * batch_size, ..., head_dim, src_length)
+
+        attn_score = torch.matmul(query, key) / math.sqrt(self.head_dim)
+        # attn_score = (
+        #     query @ key
+        # ) / self.head_dim**0.5  # (num_heads * batch_size, ..., tgt_length, src_length)
+
+        if self.mask:
+            mask = torch.ones(
+                tgt_length, src_length, dtype=torch.bool, device=query.device
+            ).tril()  # lower triangular part of the matrix
+            attn_score.masked_fill_(~mask, -torch.inf)  # fill in-place
+
+        attn_score = torch.softmax(attn_score, dim=-1)
+        # out = attn_score @ value  # (num_heads * batch_size, ..., tgt_length, head_dim)
+        out = torch.matmul(attn_score, value)
+        out = torch.cat(
+            torch.split(out, batch_size, dim=0), dim=-1
+        )  # (batch_size, ..., tgt_length, head_dim * num_heads = model_dim)
+
+        out = self.out_proj(out)
+
+        return out
+
+
+class RMSNorm(torch.nn.Module):
+    def __init__(self, dim: int, eps: float = 1e-6):
+        super().__init__()
+        self.eps = eps
+        self.weight = nn.Parameter(torch.ones(dim))
+
+    def _norm(self, x):
+        return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
+
+    def forward(self, x):
+        output = self._norm(x.float()).type_as(x)
+        return output * self.weight
+
+
+
+class SelfAttentionLayer(nn.Module):
+    def __init__(
+        self, model_dim, feed_forward_dim=2048, num_heads=8, dropout=0, mask=False
+    ):
+        super().__init__()
+
+        self.attn = AttentionLayer(model_dim, num_heads, mask)
+        self.feed_forward = nn.Sequential(
+            nn.Linear(model_dim, feed_forward_dim),
+            nn.ReLU(inplace=True),
+            nn.Linear(feed_forward_dim, model_dim),
+        )
+        self.ln1 = nn.LayerNorm(model_dim)
+        self.ln2 = nn.LayerNorm(model_dim)
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+
+    def forward(self, x, dim=-2):
+        x = x.transpose(dim, -2)
+        # x: (batch_size, ..., length, model_dim)
+        residual = x
+        out = self.attn(x, x, x)  # (batch_size, ..., length, model_dim)
+        out = self.dropout1(out)
+        out = self.ln1(residual + out)
+
+        residual = out
+        out = self.feed_forward(out)  # (batch_size, ..., length, model_dim)
+        out = self.dropout2(out)
+        out = self.ln2(residual + out)
+
+        out = out.transpose(dim, -2)
+        return out
+
+def timestep_embedding(timesteps, dim, max_period=10000):
+    """
+    Create sinusoidal timestep embeddings.
+    :param timesteps: a 1-D Tensor of N indices, one per batch element.
+                      These may be fractional.
+    :param dim: the dimension of the output.
+    :param max_period: controls the minimum frequency of the embeddings.
+    :return: an [N x dim] Tensor of positional embeddings.
+    """
+    half = dim // 2
+    freqs = torch.exp(
+        -math.log(max_period) * torch.arange(start=0, end=half, dtype=torch.float32) / half
+    ).to(device=timesteps.device)
+    args = timesteps[:, None].float() * freqs[None]
+    embedding = torch.cat([torch.cos(args), torch.sin(args)], dim=-1)
+    if dim % 2:
+        embedding = torch.cat([embedding, torch.zeros_like(embedding[:, :1])], dim=-1)
+    return embedding
+
+
+class TokenEmbedding(nn.Module):
+    def __init__(self, input_dim, embed_dim, norm_layer=None):
+        super().__init__()
+        self.token_embed = nn.Linear(input_dim, embed_dim, bias=True)
+        self.norm = norm_layer(embed_dim) if norm_layer is not None else nn.Identity()
+
+    def forward(self, x):
+        x = self.token_embed(x)
+        x = self.norm(x)
+        return x
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, embed_dim, max_len=100):
+        super(PositionalEncoding, self).__init__()
+        pe = torch.zeros(max_len, embed_dim).float()
+        pe.require_grad = False
+
+        position = torch.arange(0, max_len).float().unsqueeze(1)
+        div_term = (torch.arange(0, embed_dim, 2).float() * -(math.log(10000.0) / embed_dim)).exp()
+
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        return self.pe[:, :x.size(1)].unsqueeze(2).expand_as(x).detach()
+
+class MambaLayer(nn.Module):
+    def __init__(self, dim, d_state=64, d_conv=4, expand=2):
+        super().__init__()
+        self.dim = dim
+        self.norm = nn.LayerNorm(dim)
+        self.mamba = Mamba(
+            d_model=dim,  # Model dimension d_model
+            d_state=d_state,  # SSM state expansion factor
+            d_conv=d_conv,  # Local convolution width
+            expand=expand  # Block expansion factor
+        )
+        self.norm1 = nn.LayerNorm(dim)
+    def forward(self, x):
+        batch_size, in_steps, num_nodes, model_dim = x.shape
+        x_flat = x.reshape(batch_size, -1, model_dim)
+        x_norm = self.norm(x_flat)
+        x_mamba = self.norm1(self.mamba(x_norm))
+        out = x_mamba.reshape(batch_size, in_steps, num_nodes, model_dim)
+        return out
+
+class STMambaSync(nn.Module):
+    def __init__(
+        self,
+        num_nodes,
+        in_steps=12,
+        out_steps=12,
+        steps_per_day=288,
+        input_dim=3,
+        output_dim=1,
+        input_embedding_dim=24,
+        tod_embedding_dim=24,
+        dow_embedding_dim=24,
+        spatial_embedding_dim=0,
+        adaptive_embedding_dim=80,
+        feed_forward_dim=256,
+        num_heads=4,
+        num_layers=3,
+        dropout=0.1,
+        use_mixed_proj=True,
+    ):
+        super().__init__()
+
+
+
+        self.num_nodes = num_nodes
+        self.in_steps = in_steps
+        self.out_steps = out_steps
+        self.steps_per_day = steps_per_day
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.input_embedding_dim = input_embedding_dim
+        self.tod_embedding_dim = tod_embedding_dim
+        self.dow_embedding_dim = dow_embedding_dim
+        self.spatial_embedding_dim = spatial_embedding_dim
+        self.adaptive_embedding_dim = adaptive_embedding_dim
+        self.model_dim = (
+            input_embedding_dim
+            + tod_embedding_dim
+            + dow_embedding_dim
+            + spatial_embedding_dim
+            + adaptive_embedding_dim
+        )
+        self.num_heads = num_heads
+        self.num_layers = num_layers
+        self.use_mixed_proj = use_mixed_proj
+
+        self.input_proj = nn.Linear(input_dim, input_embedding_dim)
+        self.input_embeding = TokenEmbedding(input_dim, input_embedding_dim)
+        self.input_position_encoding = PositionalEncoding(input_embedding_dim)
+
+
+        if tod_embedding_dim > 0:
+            self.tod_embedding = nn.Embedding(steps_per_day, tod_embedding_dim)
+            self.tod_position_encoding = PositionalEncoding(tod_embedding_dim)
+        if dow_embedding_dim > 0:
+            self.dow_embedding = nn.Embedding(7, dow_embedding_dim)
+            self.dow_position_encoding = PositionalEncoding(dow_embedding_dim)
+        if spatial_embedding_dim > 0:
+            self.node_emb = nn.Parameter(
+                torch.empty(self.num_nodes, self.spatial_embedding_dim)
+            )
+            nn.init.xavier_uniform_(self.node_emb)
+            self.node_position_encoding = PositionalEncoding(spatial_embedding_dim)
+        if adaptive_embedding_dim > 0:
+            self.adaptive_embedding = nn.init.xavier_uniform_(
+                nn.Parameter(torch.empty(in_steps, num_nodes, adaptive_embedding_dim))
+            )
+
+        self.dummy_rep_token = torch.nn.Parameter(torch.randn(in_steps, num_nodes, adaptive_embedding_dim))
+
+        if use_mixed_proj:
+            self.output_proj = nn.Linear(
+                in_steps * self.model_dim, out_steps * output_dim
+            )
+        else:
+            self.temporal_proj = nn.Linear(in_steps, out_steps)
+            self.output_proj = nn.Linear(self.model_dim, self.output_dim)
+
+        self.attn_layers_t = nn.ModuleList(
+            [
+                SelfAttentionLayer(self.model_dim, feed_forward_dim, num_heads, dropout)
+                for _ in range(num_layers)
+            ]
+        )
+
+        self.attn_layers_s = nn.ModuleList(
+            [
+                SelfAttentionLayer(self.model_dim, feed_forward_dim, num_heads, dropout)
+                for _ in range(num_layers)
+            ]
+        )
+        self.S_position_encoding = PositionalEncoding(self.model_dim)
+        self.T_position_encoding = PositionalEncoding(self.model_dim)
+
+        self.norm = nn.LayerNorm(self.model_dim)
+        self.sssm = MambaLayer(self.model_dim, d_state=32, d_conv=4, expand=2)
+
+    def forward(self, x, timesteps=None):
+        # x: (batch_size, in_steps, num_nodes, input_dim+tod+dow=3)
+        batch_size = x.shape[0]
+
+        if self.tod_embedding_dim > 0:
+            tod = x[..., 1]
+        if self.dow_embedding_dim > 0:
+            dow = x[..., 2]
+        x = x[..., :self.input_dim]
+
+        x = self.input_embeding(x)
+
+        features = [x]
+        if self.tod_embedding_dim > 0:
+            tod_emb = self.tod_embedding(
+                (tod * self.steps_per_day).long()
+            )  # (batch_size, in_steps, num_nodes, tod_embedding_dim)
+            features.append(tod_emb)
+        if self.dow_embedding_dim > 0:
+            dow_emb = self.dow_embedding(
+                dow.long()
+            )  # (batch_size, in_steps, num_nodes, dow_embedding_dim)
+            features.append(dow_emb)
+        if self.spatial_embedding_dim > 0:
+            spatial_emb = self.node_emb.expand(
+                batch_size, self.in_steps, *self.node_emb.shape
+            )
+            features.append(spatial_emb)
+        if self.adaptive_embedding_dim > 0:
+            adp_emb = self.adaptive_embedding.expand(
+                size=(batch_size, *self.adaptive_embedding.shape)
+            )
+            features.append(adp_emb)
+
+        x = torch.cat(features, dim=-1)  # (batch_size, in_steps, num_nodes, model_dim)
+
+        # Create the directory if it doesn't exist
+
+
+
+        # Visualize and save the Isomap plot for the input tensor
+
+        # Visualize and save the input tensor
+        # plt.figure(figsize=(12, 4))
+        # plt.imshow(x[0, :, :, 0].detach().cpu().numpy())  # Visualize the first channel of the first batch
+        # plt.title("Input Tensor")
+        # plt.colorbar()
+        # plt.savefig(os.path.join(output_dir, f"Input_tensor.png"))
+        # plt.close()
+        # Save the initial tensor
+        np.savez(( "initial.npy"), x.detach().cpu().numpy())
+
+        input = x
+
+
+
+        for attn in self.attn_layers_t:
+            x = attn(x, dim=1)
+        for attn in self.attn_layers_s:
+            x = attn(x, dim=2)
+        # Visualize and save the tensor after applying attention
+        # plt.figure(figsize=(12, 4))
+        # plt.imshow(x[0, :, :, 0].detach().cpu().numpy())  # Visualize the first channel of the first batch
+        # plt.title("Tensor after Attention")
+        # plt.colorbar()
+        # plt.savefig(os.path.join(output_dir, f"Tensor_after_attention.png"))
+        # plt.close()
+
+
+        #x = self.sssm(x)
+
+
+
+
+        # Visualize and save the tensor after applying Mamba layer
+        # plt.figure(figsize=(12, 4))
+        # plt.imshow(x[0, :, :, 0].detach().cpu().numpy())  # Visualize the first channel of the first batch
+        # plt.title("Tensor after Mamba")
+        # plt.colorbar()
+        # plt.savefig(os.path.join(output_dir, f"Tensor_after_mamba.png"))
+        # plt.close()
+
+
+
+
+        if self.use_mixed_proj:
+            out = x.transpose(1, 2)  # (batch_size, num_nodes, in_steps, model_dim)
+            out = out.reshape(
+                batch_size, self.num_nodes, self.in_steps * self.model_dim
+            )
+            out = self.output_proj(out).view(
+                batch_size, self.num_nodes, self.out_steps, self.output_dim
+            )
+            out = out.transpose(1, 2)  # (batch_size, out_steps, num_nodes, output_dim)
+        else:
+            out = x.transpose(1, 3)  # (batch_size, model_dim, num_nodes, in_steps)
+            out = self.temporal_proj(
+                out
+            )  # (batch_size, model_dim, num_nodes, out_steps)
+            out = self.output_proj(
+                out.transpose(1, 3)
+            )  # (batch_size, out_steps, num_nodes, output_dim)
+
+        return out
+
+
+# if __name__ == "__main__":
+#     model = STMambaSync(207, 12, 12)
+#     device = torch.device('cuda')  # or torch.device('cuda:X') for a specific GPU
+#     model = model.to(device)  # Move the model to the CUDA device
+#
+#     # Create a sample input tensor
+#     x = torch.randn(64, 12, 207, 3)
+#     x = x.to(device)  # Move the input tensor to the CUDA device
+#
+#     summary(model, [64, 12, 207, 3])
